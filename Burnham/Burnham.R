5dd9cb47e8017b285d37a80890029a2aabc9de5b
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-03-03 12:30:48 -0700

    Add files via upload

---
+++ Burnham/Burnham.R
@@ -0,0 +1,23 @@
+
+#======== Text Mine Captain Burnham on Star Trek DISCO
+
+# http://www.chakoteya.net/STDisco17/episodes.html
+
+# SEASON - EPISODE
+# 1 - 1
+# 1 - 2
+# 1 - 3
+# 1 - 4
+# 1 - 5
+# 1 - 6
+
+
+
+# burnhamSpeech1 = season 1
+
+
+
+library(tidyverse)
+library(stringi)
+library(tidytext)
+
