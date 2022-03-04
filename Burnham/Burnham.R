2b6e022790f02a71bb68b5719e1240b601d89eab
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-03-03 18:07:32 -0700

    wordclouds

--- Burnham/Burnham.R
+++ Burnham/Burnham.R
@@ -4,12 +4,7 @@
 # http://www.chakoteya.net/STDisco17/episodes.html
 
 # SEASON - EPISODE
-# 1 - 1
-# 1 - 2
-# 1 - 3
-# 1 - 4
-# 1 - 5
-# 1 - 6
+# 1 - 1, 1 - 2, 1 - 3, 1 - 4, 1 - 5, 1 - 6, 1- 7, 1- 8, 1 - 9
 
 
 
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 # TrekDISCO
 a NLP project for Captain Michael Burnham of the Star Trek Discovery
+
+
+## Season 1 (R vs Python wordcloud)
+![R](img/burnham_wordcloud.png)
+
+![Python](img/burnham1_python)
