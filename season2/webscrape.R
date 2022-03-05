3fc554e0982bddc4ea0197f806b203bfb4b5c74a
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2022-03-05 11:00:48 -0700

    seasons 2-4

--- season2/webscrape.R
+++ season2/webscrape.R
@@ -1,11 +1,23 @@
 
-# episode 1 -- season 2
+# latest season 4 - episode 10 (Mar 5, 2022)
 
 library(tidyverse)
 library(rvest)
 
+url = 'https://transcripts.foreverdreaming.org/viewtopic.php?f=987&t=64730'
 
-trek = read_html('https://transcripts.foreverdreaming.org/viewtopic.php?f=987&t=54700')
+trek = read_html(url)
 
-words = trek %>% html_elements('.postbody') %>% html_text2()
-words
+words = trek %>% html_elements('.postbody') %>% html_text2() %>% 
+  str_remove_all(pattern = "push") %>% 
+  str_replace_all("window.adsbygoogle", replacement = "") %>% 
+  str_replace_all("adsbygoogle", replacement = "") %>%
+  str_remove_all("Transcripts Topic Top") %>% 
+  str_remove_all("[:punct:]") %>% 
+  str_remove_all("[:symbol:]")
+
+# words
+
+write_file(words,  path = './season4/disco4-10.txt')
+
+
